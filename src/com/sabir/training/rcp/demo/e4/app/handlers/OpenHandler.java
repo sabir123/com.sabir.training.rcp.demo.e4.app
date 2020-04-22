3daaf48ab24ca05beb5f099428634a8e92b4b2f9
sabir123 <sabirleads@gmail.com>
2020-04-22 20:36:10 +0300

    E4 demo app checkin
    E4 demo app checkin

---
+++ src/com/sabir/training/rcp/demo/e4/app/handlers/OpenHandler.java
@@ -0,0 +1,13 @@
+package com.sabir.training.rcp.demo.e4.app.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
+public class OpenHandler {
+
+	@Execute
+	public void execute(Shell shell){
+		FileDialog dialog = new FileDialog(shell);
+		dialog.open();
+	}
+}
